b86436bddc2d99359902de05fad417a1a79c0b98
User <User@1-PC>
2024-11-24 12:06:09 +0500

    Добавьте файлы проекта.

---
+++ HumanResourcesManagement/EmployeeEditPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HumanResourcesManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeEditPage.xaml
+    /// </summary>
+    public partial class EmployeeEditPage : Page
+    {
+        private Employee _employee;
+        public EmployeeEditPage(Employee employee)
+        {
+            InitializeComponent();
+            _employee = employee;
+            NameEmployee.DataContext = _employee;
+            PositionEmployee.DataContext = _employee;
+        }
+
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            _employee.Name = NameEmployee.Text;
+            _employee.Position = PositionEmployee.Text;
+
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ HumanResourcesManagement/EmployeeListPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HumanResourcesManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeListPage.xaml
+    /// </summary>
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+
+    public partial class EmployeeListPage : Page
+    {
+        public EmployeeListPage()
+        {
+            InitializeComponent();
+            var employees = new List<Employee>
+            {
+                new Employee { ID = 1, Name = "Иван", Position = "Программист" },
+                new Employee { ID = 2, Name = "Сергей", Position = "Менеджер" },
+                new Employee { ID = 3, Name = "Петр", Position = "Дизайнер" },
+                new Employee { ID = 4, Name = "Богдан", Position = "Программист" },
+                new Employee { ID = 5, Name = "Владислав", Position = "Дизайнер" }
+            };
+            DataGrid.ItemsSource = employees;
+        }
+
+        private void EditEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid.SelectedItem is Employee selectedEmployee)
+            {
+                NavigationService.Navigate(new EmployeeEditPage(selectedEmployee));
+            }
+        }
+    }
+}
